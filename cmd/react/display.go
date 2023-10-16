af5ab6cec6f673cadd881a77f192890c56ac5236
horita-yuya <horitayuya@gmail.com>
2023-10-16 21:46:16 +0900

    formatted jsx props

--- cmd/react/display.go
+++ cmd/react/display.go
@@ -50,32 +50,56 @@ func formatJSX(jsx JSXElement, indent string) string {
 	} else {
 		formatted = fmt.Sprintf("%s<%s key=%s", indent, jsx.Type, jsx.Key)
 	}
+
+	propsIndent := "  "
+	jsxIndent := "  "
+	jsxCount := 0
+	childrenCount := 0
 	for k, v := range jsx.Props {
 		if k == "children" {
+			childrenCount += 1
 			continue
 		} else {
-			formatted += fmt.Sprintf(" %s=%v", k, v)
+			switch v.(type) {
+			case JSXElement:
+				jsxCount += 1
+				formatted += fmt.Sprintf("\n%s%s={\n%v\n%s}\n", propsIndent+indent, k, formatJSX(v.(JSXElement), jsxIndent+propsIndent+indent), propsIndent+indent)
+			default:
+				formatted += fmt.Sprintf("\n%s%s={%v}", propsIndent+indent, k, v)
+			}
 		}
 	}
-	formatted += fmt.Sprintf(">")
+
+	if len(jsx.Props) == 0 || len(jsx.Props) == 1 && childrenCount == 1 {
+		formatted += fmt.Sprintf(">")
+	} else if len(jsx.Props) == jsxCount {
+		formatted += fmt.Sprintf("\n%s>", propsIndent+indent)
+	} else {
+		formatted += fmt.Sprintf("\n%s>", indent)
+	}
+
 	children := jsx.Props["children"]
 	if children != nil {
-		childIndent := "  "
 		switch children.(type) {
 		case []interface{}:
 			for _, child := range children.([]interface{}) {
 				switch child.(type) {
 				case JSXElement:
-					formatted += fmt.Sprintf("\n%v\n", formatJSX(child.(JSXElement), childIndent+indent))
+					formatted += fmt.Sprintf("\n%v\n", formatJSX(child.(JSXElement), jsxIndent+indent))
 				default:
-					formatted += fmt.Sprintf("\n%s%v\n", childIndent+indent, child)
+					formatted += fmt.Sprintf("\n%s%v\n", jsxIndent+indent, child)
 				}
 			}
 		case JSXElement:
-			formatted += fmt.Sprintf("\n%v\n", formatJSX(children.(JSXElement), childIndent+indent))
+			formatted += fmt.Sprintf("\n%v\n", formatJSX(children.(JSXElement), jsxIndent+indent))
 		default:
-			formatted += fmt.Sprintf("\n%s%v\n", indent+childIndent, children)
+			formatted += fmt.Sprintf("\n%s%v\n", indent+jsxIndent, children)
 		}
+	} else {
+		// align heads of closing tags.
+		// <div>
+		// </div>
+		formatted += "\n"
 	}
 	formatted += fmt.Sprintf("%s</%s>", indent, jsx.Type)
 
